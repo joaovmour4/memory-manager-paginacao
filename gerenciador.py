dd9445533821fb82b5265f15de9ae23e342affc5
joaov.mour <joaov.mour@unifesspa.edu.br>
2023-03-13 14:27:10 -0300

    teste funções

--- gerenciador.py
+++ gerenciador.py
@@ -1,13 +1,24 @@
 def inserir(processo, lista):
+    for i in range(len(lista)):
+        if type(lista[i]) == int:
+            if lista[i] == processo[0]:
+                    lista.insert(i, processo)
+                    break
+                    lista.insert(0, processo)
     for endereco in processo:
         lista.remove(endereco)
-    for i in range(len(lista)):
-        if lista[i] > processo[len(processo)-1]:
-            if i != 0:
-                lista.insert(i-1, processo)
-                break
-            else:
-                lista.insert(0, processo)
+
+def verificar_espaco(size, array):
+    sizeAux = 0
+    for i in array:
+        if type(i) != list:
+            sizeAux += 1
+        else:
+            sizeAux = 0
+        if sizeAux == size:
+            return i - (size-1)
+    return False
+        
 
 
 memory = [i for i in range(0, 100)]
@@ -18,6 +29,13 @@ inserir(process, memory)
 
 process = [i for i in range(14, 20)]
 
-# inserir(process, memory)
+inserir(process, memory)
+
+process = [i for i in range(0, 10)]
+
+inserir(process, memory)
+
 
 print(memory)
+
+print(verificar_espaco(8, memory))
