8796f95cea0e45a88cb33dba0ae50a1cc3e9db1f
joaov.mour <joaov.mour@unifesspa.edu.br>
2023-03-13 23:04:23 -0300

    second commit

--- gerenciador.py
+++ gerenciador.py
@@ -1,10 +1,11 @@
+from processo import Processo
+
 def inserir(processo, lista):
     for i in range(len(lista)):
         if type(lista[i]) == int:
             if lista[i] == processo[0]:
                     lista.insert(i, processo)
                     break
-                    lista.insert(0, processo)
     for endereco in processo:
         lista.remove(endereco)
 
@@ -18,24 +19,50 @@ def verificar_espaco(size, array):
         if sizeAux == size:
             return i - (size-1)
     return False
+
+def arrPrint(array):
+    column=0
+    for element in array:
+        if type(element) == int:
+            print(element, end='\t')
+            column+=1
+        else:
+            print('[', end='')
+            for listElement in element:
+                print(listElement, end='\t')
+                column+=1
+                if column > 7:
+                    print('\n', end='')
+                    column=0
+            print(']', end='')
+        if column > 7:
+            print('\n', end='')
+            column=0
         
 
 
-memory = [i for i in range(0, 100)]
-
-process = [i for i in range(10, 14)]
-
-inserir(process, memory)
-
-process = [i for i in range(14, 20)]
-
-inserir(process, memory)
-
-process = [i for i in range(0, 10)]
-
-inserir(process, memory)
+memory = ['-' for i in range(0, 64)]
+arrProcess = []
 
 
-print(memory)
+arrProcess.append(Processo.insertList(arrProcess))
+arrProcess.append(Processo.insertList(arrProcess))
 
-print(verificar_espaco(8, memory))
+
+column = 0
+
+for adrMemory in range(len(memory)):
+    for process in arrProcess:
+        if process.locInit == adrMemory:
+            for i in range(process.locInit, process.locInit + process.size):
+                memory[i] = process.id
+    print(memory[adrMemory], end='\t')
+    column+=1
+    if column == 8:
+        column=0
+        print()
+
+print(arrProcess[0].locInit, arrProcess[0].size)
+    
+
+# print(verificar_espaco(8, memory))
